628c44a4057cff65b6ff550bdbf32f3fd873e9b7
Sys4 <metelkin_2007@mail.ru>
2025-02-11 18:02:39 +0900

    4-задание(дома доделаю)

--- server.py
+++ server.py
@@ -52,6 +52,45 @@ def return_sample_page():
                       </body>
                     </html>"""
 
+@app.route('/promotion_image')
+def return_promotion_image():
+    k = []
+    i = 0
+    while True:
+        if i == 0:
+            k.append('<h2>Человечество вырастает из детства.</h2></br>')
+            i += 1
+        elif i == 1:
+            k.append('Человечеству мала одна планета.</br>')
+            i += 1
+        elif i == 2:
+            k.append('Мы сделаем обитаемыми безжизненные пока планеты.</br>')
+            i += 1
+        elif i == 3:
+            k.append('И начнем с Марса!</br>')
+            i += 1
+        elif i == 4:
+            k.append('Присоединяйся!')
+            break
+        else:
+            break
+
+    promotion_text = ''.join(k)
+
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Колонизация!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='image_mars/img.png')}" alt="здесь должна была быть картинка, но не нашлась">
+                    <div>{promotion_text}</div>
+                  </body>
+                </html>"""
+
 
 if __name__ == '__main__':
     app.run(port=9000, host='127.0.0.1')
